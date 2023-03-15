e55db4dad46c3808747112780dc80484d5d0217f
Ashkan Rezaee <101568776+AshkanHL@users.noreply.github.com>
2023-03-15 16:30:28 +0100

    Weather Report

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import WeatherComponent from './weatherComponent';
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Henning Larsen</h1>
+      <h1>Weather Report</h1>
       <WeatherComponent/>
     </div>
   );
